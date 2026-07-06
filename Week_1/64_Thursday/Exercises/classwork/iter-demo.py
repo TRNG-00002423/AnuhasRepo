a40a2116cabf62628b62a315d23e61e64f59b538
StumbledUpon-CS <anuhagayam@gmail.com>
2026-07-06 14:19:43 -0700

    Completed Lab: Python Iterators And Iterables

---
+++ Week_1/64_Thursday/Exercises/classwork/iter-demo.py
@@ -0,0 +1,21 @@
+numbers=[1,2,3,4,5]
+
+print("")
+print("")
+
+for number in numbers:
+    print(number)
+
+print("")
+
+
+print("-----------------------------------")
+my_iter=iter(numbers)
+
+print("")
+
+print(next(my_iter))
+print(next(my_iter))
+
+print("")
+print("")
