e0efb09ef73acf83d5883780a8454767e3e42a14
StumbledUpon-CS <anuhagayam@gmail.com>
2026-07-06 14:18:44 -0700

    Completed Lab: Python Tuples

---
+++ Week_1/64_Thursday/Exercises/classwork/tuple-demo.py
@@ -0,0 +1,35 @@
+# Tuple : ordered, immutable
+
+print(" ")
+print(" ")
+
+
+empty_tuple = ()
+
+rgb_color=(255,255,0)
+
+
+coordinates=(10,20)
+
+
+def results(marks):
+
+    # Based on marks returns the percentage
+
+    # Based on percentage calculated return the grade
+
+    Percentage=85
+    grade="B"
+
+    return (Percentage, grade)
+
+
+my_result = results(450)
+
+
+my_result1=(85,"A")
+
+percentage, grade=my_result1
+
+print(" ")
+print(" ")
